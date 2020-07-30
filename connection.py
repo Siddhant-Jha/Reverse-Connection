4728d76b33c90a5e323409d1b41886d21dc74b9b
Siddhant-Jha <siddhant1751@gmail.com>
2020-07-30 16:09:14 +0530

    Added Print Statement In AcceptConnection Instance and Created SendData and RecieveData Instances

--- connection.py
+++ connection.py
@@ -15,4 +15,14 @@ class serverConnection:
 
     def AcceptConnection (self):
         self.client_conn, self.client_address = self.socket.accept()
+        print("[+] Connection Established With" + self.client_address[0] + "On Port" + self.client_address[1])
         return (self.client_conn, self.client_address)
+
+    def SendData (self, user_input):
+        user_input = bytes(user_input, "utf-8")
+        self.client_conn.send(user_input)
+
+    def recieve_data (self):
+        recieve_data = self.client_conn.recv(1024)
+        self_data = recieve_data.decode('utf-8')
+        return self_data
